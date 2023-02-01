e9e64b094efb76f04639f1e11b92805dacb62620
chinmayrelkar <chinmay@cohesive.so>
2023-02-02 01:00:52 +0530

    adds logs for auth headers

--- cohesive_marketplace_django/auth.py
+++ cohesive_marketplace_django/auth.py
@@ -11,11 +11,14 @@ class AuthMiddleware:
 
     def __call__(self, request):
         # Get the Authorization header
+        print(request.headers)
         authorization_header = request.headers.get('authorization')
         if not authorization_header or authorization_header == "":
             authorization_header = request.META.get('HTTP_AUTHORIZATION')
             if not authorization_header or authorization_header == "":
-                return HttpResponse(status=http.HTTPStatus.UNAUTHORIZED)
+                authorization_header = request.headers.get('Authorization')
+                if not authorization_header or authorization_header == "":  
+                    return HttpResponse(status=http.HTTPStatus.UNAUTHORIZED)
 
         # Get the token from the header
         token = authorization_header.replace('Bearer ', '')
